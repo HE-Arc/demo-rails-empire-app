c8b3028f4674869a729f7614332853d58ec5ac81
Steve Visinand <visinandst@gmail.com>
2018-01-10 12:57:08 +0100

    Add Trooper validation

--- app/models/trooper.rb
+++ app/models/trooper.rb
@@ -1,3 +1,13 @@
 class Trooper < ApplicationRecord
+    validates :name,
+        presence: true, 
+        uniqueness: true
 
+    validates :age,
+        numericality: { greater_than_or_equal_to: 0 },
+        presence: true
+
+    validates :strength,
+        numericality: { greater_than: 0, less_than_or_equal_to: 10 },
+        presence: true
 end
