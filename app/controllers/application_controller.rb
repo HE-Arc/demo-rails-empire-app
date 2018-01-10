ae10812395ba0f049aed9c3acca007870f7aa86b
Steve Visinand <visinandst@gmail.com>
2018-01-10 15:23:55 +0100

    Add root page, with "sith of the day"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def index
+    @sith_of_the_day = Category.find_by(:name => 'Sith').trooper.order("RAND()").first
+  end
+
 end
