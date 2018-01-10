94fbd6c57d291b4aeaceb6bd9c3589c85969ccce
Steve Visinand <visinandst@gmail.com>
2018-01-10 12:51:13 +0100

    Add trooper insertion view

--- app/controllers/troopers_controller.rb
+++ app/controllers/troopers_controller.rb
@@ -2,4 +2,23 @@ class TroopersController < ApplicationController
   def index
     @troopers = Trooper.all
   end
+
+  def new
+    @trooper = Trooper.new
+  end
+
+  def create
+    @trooper = Trooper.new(trooper_params)
+    if @trooper.save
+      redirect_to troopers_path, notice: @trooper.name + ' at your orders !'
+    else
+      render :new
+    end
+  end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def trooper_params()
+      params.require(:trooper).permit(:name, :age, :strength)
+    end
 end
