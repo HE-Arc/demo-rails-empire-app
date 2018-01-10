9149a63ce5b9c0c8a3dc1b24aedbfad3b2ab8c9b
Steve Visinand <visinandst@gmail.com>
2018-01-10 15:27:25 +0100

    Add "trooper of the day" method in category model.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    @sith_of_the_day = Category.find_by(:name => 'Sith').trooper.order("RAND()").first
+    @sith_of_the_day = Category.find_by(:name => 'Sith').trooper_of_the_day
   end
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
     has_many :trooper
+
+    def trooper_of_the_day
+        trooper.order("RAND()").first
+    end
 end
